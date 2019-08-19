d1039ea84421ea41eefc8c45b65b9dd1027b46e0
Daniel Ruf <d.ruf@bitexpert.de>
2019-08-19 07:58:34 +0200

    Sort by similarity and path - closes #3

--- index.js
+++ index.js
@@ -21,13 +21,30 @@ if (process.argv[2]) {
   const fileOrig = fs.readFileSync(givenFile, { encoding: 'utf8' });
 
   (async () => {
+    const resultSet = [];
     for await (const file of getFiles(path.resolve(process.argv[3]))) {
       const fileName = path.basename(file);
       if (givenFileName === fileName && fs.lstatSync(file).isFile()) {
         const fileContent = fs.readFileSync(file, { encoding: 'utf8' });
         const similarity = stringSimilarity.compareTwoStrings(fileOrig, fileContent);
-        console.log(`${similarity} => ${file}`);
+        resultSet.push(
+          {
+            similarity,
+            file
+          }
+        )
       }
+    }
+    if (resultSet.length) {
+      resultSet.sort(function(a, b){
+        if (a.similarity !== b.similarity) {
+          return b.similarity - a.similarity;
+        }
+        return a.file.localeCompare(b.file);
+      })
+      resultSet.forEach(result => {
+        console.log(`${result.similarity} => ${result.file}`);
+      });
     }
   })()
 }
